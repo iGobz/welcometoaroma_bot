d2d2e67def883c66fcc66ff7d461b0964131d060
Ivan Kuznetsov <medvedmedved@gmail.com>
2022-04-05 00:49:48 +0700

    another example test

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,7 +1,22 @@
 import fetch from 'node-fetch';
 
-const fn = () => {
-    const res = fetch('https://api.telegram.org/bot5242713931:AAEHEFHsmGlaWLKkX1l_LRoNG7Kzm1dvWbM/sendMessage', {
+// const fn = () => {
+//     const res = fetch('https://api.telegram.org/bot5242713931:AAEHEFHsmGlaWLKkX1l_LRoNG7Kzm1dvWbM/sendMessage', {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             chat_id: 218026127,
+//             text: "Test from fetch"
+//         })
+//     }).then((data) => {
+//         console.log(data);
+//     }).catch((error) => {
+//         console.log(error);
+//     });  
+// };
+
+exports.handler = async (event) => {
+
+    return fetch('https://api.telegram.org/bot5242713931:AAEHEFHsmGlaWLKkX1l_LRoNG7Kzm1dvWbM/sendMessage', {
         method: 'POST',
         body: JSON.stringify({
             chat_id: 218026127,
@@ -9,24 +24,17 @@ const fn = () => {
         })
     }).then((data) => {
         console.log(data);
-    }).catch((error) => {
-        console.log(error);
-    });  
-};
 
-export const handler = async (event) => {
-    try {
-        fn();
-        const subject = event.queryStringParameters.name || 'World'
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: `Hello ${subject}` }),
-            // // more keys you can return:
-            // headers: { "headerName": "headerValue", ... },
-            // isBase64Encoded: true,
+        try {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: `Hello ${subject}` }),
+            }
+        } catch (error) {
+            console.log(error);
+            return { statusCode: 500, body: error.toString() }
         }
-    } catch (error) {
-        console.log(error);
-        return { statusCode: 500, body: error.toString() }
-    }
+    
+
+    }).catch(error => ({ statusCode: 422, body: String(error) })); 
 }
