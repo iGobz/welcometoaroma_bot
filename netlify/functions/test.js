e7a270f3090b9dd9d8dac1b8fe611441f64be9e1
Ivan Kuznetsov <medvedmedved@gmail.com>
2022-04-05 00:34:04 +0700

    handler added

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,15 +1,34 @@
-//const fetch = require('node-fetch').default;
 import fetch from 'node-fetch';
 
+const fn = () => {
+    const res = fetch('https://api.telegram.org/bot5242713931:AAEHEFHsmGlaWLKkX1l_LRoNG7Kzm1dvWbM/sendMessage', {
+        method: 'POST',
+        body: JSON.stringify({
+            chat_id: 218026127,
+            text: "Test from fetch"
+        })
+    }).then((data) => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(error);
+    });  
+};
 
-const res = fetch('https://api.telegram.org/bot5242713931:AAEHEFHsmGlaWLKkX1l_LRoNG7Kzm1dvWbM/sendMessage', {
-    method: 'POST',
-    body: JSON.stringify({
-        chat_id: 218026127,
-        text: "Test from fetch"
-    })
-}).then((data) => {
-    console.log(data);
-}).catch((error) => {
-    console.log(error);
-});
+const handler = async (event) => {
+    try {
+        fn();
+        const subject = event.queryStringParameters.name || 'World'
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: `Hello ${subject}` }),
+            // // more keys you can return:
+            // headers: { "headerName": "headerValue", ... },
+            // isBase64Encoded: true,
+        }
+    } catch (error) {
+        console.log(error);
+        return { statusCode: 500, body: error.toString() }
+    }
+}
+
+export default handler;
