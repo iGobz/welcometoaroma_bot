d3a497153f8843329b8bf591e06626f57a6492f1
Ivan Kuznetsov <medvedmedved@gmail.com>
2022-04-05 14:43:37 +0700

    Убрано закомментированное

--- netlify/functions/update-b8eca/update-b8eca.js
+++ netlify/functions/update-b8eca/update-b8eca.js
@@ -4,30 +4,8 @@ import fetch from 'node-fetch';
 
 
 const token = process.env.BOT_TOKEN || 'token';
-process.env.NTBA_FIX_319 = 1;
 
-// console.log(process.env.BOT_TOKEN);
 const bot = new Telegram(token)
-
-// console.log(token)
-
-// const start = async () => {
-//     try {
-//         // const resp = await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
-//         //     chat_id : 218026127,
-//         //     text: 'Тест axios!',
-//         // });
-//         // console.log('First response: ', resp);
-
-//         const res = await bot.sendMessage(218026127, 'Тест sendMessage!');
-//         console.log('Hello res: ', res);
-
-//     } catch(error) {
-//         console.log(error);
-//     }   
-// };
-
-// start();
 
 
 
@@ -43,18 +21,6 @@ bot.onText(/\/start/, async (msg, match) => {
     const res = await bot.sendMessage(chatId, 'Привет! Я Арома Бот!');
 });
 
-// bot.on('message', async (msg, meta) => {
-//     const chatId = msg.chat.id;
-//     console.log('Got message at ', chatId);
-//     bot.sendMessage(chatId, 'Got message');
-//     const res = await bot.sendMessage(chatId, 'Привет медвет!');
-
-//     // await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
-//     //     chatId,
-//     //     text: 'Тест axios',
-//     // });
-//     console.log('Message res: ', res);
-// });
 
 exports.handler = async (event, context) => {
     try {
@@ -68,5 +34,3 @@ exports.handler = async (event, context) => {
         return { statusCode: 500, body: error.toString() }
     }
 }
-
-// export default { handler }
