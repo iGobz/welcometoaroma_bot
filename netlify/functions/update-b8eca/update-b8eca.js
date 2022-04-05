00e97a5f53e48f2b51866b74296bdfe319aa2420
Ivan Kuznetsov <medvedmedved@gmail.com>
2022-04-05 14:30:27 +0700

    Добавлено приветствие

--- netlify/functions/update-b8eca/update-b8eca.js
+++ netlify/functions/update-b8eca/update-b8eca.js
@@ -9,32 +9,38 @@ process.env.NTBA_FIX_319 = 1;
 // console.log(process.env.BOT_TOKEN);
 const bot = new Telegram(token)
 
-console.log(token)
+// console.log(token)
 
-const start = async () => {
-    try {
-        // const resp = await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
-        //     chat_id : 218026127,
-        //     text: 'Тест axios!',
-        // });
-        // console.log('First response: ', resp);
+// const start = async () => {
+//     try {
+//         // const resp = await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
+//         //     chat_id : 218026127,
+//         //     text: 'Тест axios!',
+//         // });
+//         // console.log('First response: ', resp);
 
-        const res = await bot.sendMessage(218026127, 'Тест sendMessage!');
-        console.log('Hello res: ', res);
+//         const res = await bot.sendMessage(218026127, 'Тест sendMessage!');
+//         console.log('Hello res: ', res);
 
-    } catch(error) {
-        console.log(error);
-    }   
-};
+//     } catch(error) {
+//         console.log(error);
+//     }   
+// };
 
-start();
+// start();
+
+
 
 bot.onText(/hello/, async (msg, match) => {
     const chatId = msg.chat.id;
-    console.log('Got hello at ', chatId);
+    console.log('Got hello at ', chatId, msg);
     const res = await bot.sendMessage(chatId, 'Привет!');
     console.log('Hello res: ', res);
+});
 
+bot.onText(/\/start/, async (msg, match) => {
+    const chatId = msg.chat.id;
+    const res = await bot.sendMessage(chatId, 'Привет! Я Арома Бот!');
 });
 
 // bot.on('message', async (msg, meta) => {
