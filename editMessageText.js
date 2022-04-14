7e031ee472dd28d8fd59950e1329967029c563ec
Ivan Kuznetsov <medvedmedved@gmail.com>
2022-04-14 17:47:04 +0700

    Loggin

--- editMessageText.js
+++ editMessageText.js
@@ -1,6 +1,8 @@
 const axios = require("axios").default;
 
 module.exports = async (chat_id, text, options) => {
+
+    console.log(options);
   await axios.post(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/editMessageText`, {
     chat_id,
     text,
